47d59a05e7fc1648bd8ced7e5c7f7a4861ed608b
DrevOliv <oliver.drevet@outlook.com>
2023-02-18 09:01:27 +0100

    Initial Commit

---
+++ PianoNoteApp/PianoNoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PianoNoteAppApp.swift
+//  PianoNoteApp
+//
+//  Created by Oliver Drevet on 2023-02-18.
+//
+
+import SwiftUI
+
+@main
+struct PianoNoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
